5f6978b0d8c36fe3887b04103398608bca576d55
Sunday <trenton.taylor.email@gmail.com>
2026-06-22 08:21:37 -0500

    sunday: Edit src/app/App.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -383,6 +383,7 @@ function App() {
             </Suspense>
             <TutorialManager />
             {hasUpdate && <VersionUpdateBanner onDismiss={dismiss} />}
+            <PlantManagerReminderToast />
             <Analytics />
             <SpeedInsights />
         </>
