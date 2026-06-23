4ce4a86be1a35d45b1010a71439951a7d82f8f81
Sunday <trenton.taylor.email@gmail.com>
2026-06-23 10:29:39 -0500

    sunday: Edit src/app/constants/myAccountConstants.js

--- src/app/constants/myAccountConstants.js
+++ src/app/constants/myAccountConstants.js
@@ -20,8 +20,6 @@ export const START_PAGE_OPTIONS = [
     { icon: 'fa-truck', id: 'Tractors' },
     { icon: 'fa-trailer', id: 'Trailers' },
     { icon: 'fa-hard-hat', id: 'Operators' },
-    { icon: 'fa-list-check', id: 'List' },
-    { icon: 'fa-file-lines', id: 'Reports' },
     { icon: 'fa-calendar-days', id: 'Plan' },
     { icon: 'fa-cogs', id: 'Heavy Equipment' },
     { icon: 'fa-truck-pickup', id: 'Pickup Trucks' }
